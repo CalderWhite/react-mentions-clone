1d7bd57ebdea0ce6e73cf0a5c706f83eaf9e8087
Calder White <calderwhite1@gmail.com>
2023-04-30 14:57:13 -0400

    Added text.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,7 +5,7 @@ import MyTextbox from '../components/MyTextbox';
 export default function Home() {
   return (
     <>
-      <p>Hi</p>
+      <p>Use the textbox below! Type "@" to mention someone and either hit tab or enter to select your entry. You may also use the arrow keys to move between entries</p>
       <MyTextbox />
     </>
   )
