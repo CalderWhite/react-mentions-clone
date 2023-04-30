59343680550bc220e055efd1014b2c141373f4e8
Calder White <calderwhite1@gmail.com>
2023-04-30 00:57:49 -0400

    added single character deletion

--- src/MyTextbox.jsx
+++ src/MyTextbox.jsx
@@ -6,7 +6,7 @@ function MyTextbox() {
   const [value, setValue] = useState('');
   const [startIndex, setStartIndex] = useState(0);
 
-  const [visibileText, setVisibleText] = useState([{start: 0, len: 0, textType: 'text'}]);
+  const [visibileText, setVisibleText] = useState([{len: 0, textType: 'text'}]);
 
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [activeSuggestion, setActiveSuggestion] = useState(0);
@@ -32,7 +32,7 @@ function MyTextbox() {
       // both of these should be within a single text item,
       // we just need to find which one, split it up and insert the suggestion inbetween the two halves.
       let found = false;
-      let newVisibleText = visibileText.slice();
+      let newVisibleText = JSON.parse(JSON.stringify(visibileText));
       let start = 0;
       for (let i=0; i<newVisibleText.length; ++i) {
         if (start <= startIndex && (start + newVisibleText[i].len) >= selectionStart) {
@@ -63,7 +63,6 @@ function MyTextbox() {
           const rightHalf = value.substring(selectionStart);
           setValue(leftHalf + suggestion + rightHalf);
 
-          console.log(newVisibleText)
           found = true;
           break;
         }
@@ -85,7 +84,7 @@ function MyTextbox() {
   const updateBackgroundBoxes = (event) => {
     // subtract one since this is the selection AFTER the character has been typed
     const selectionStart = event.target.selectionStart - 1;
-    let newVisibleText = visibileText.slice();
+    let newVisibleText = JSON.parse(JSON.stringify(visibileText));
     // find the correct item to update and then update it
     let start = 0;
     for (let i=0; i<newVisibleText.length; ++i) {
@@ -118,14 +117,47 @@ function MyTextbox() {
     setVisibleText(newVisibleText);
   }
 
+  const handleDelete = (event) => {
+    // subtract one since this is the selection AFTER the character has been typed
+    const selectionStart = event.target.selectionStart;
+    if (selectionStart === 0) {
+      return
+    }
+    let newVisibleText = JSON.parse(JSON.stringify(visibileText));
+    let start = 0;
+    for (let i=0; i<newVisibleText.length; ++i) {
+      if (start <= selectionStart && (start + newVisibleText[i].len) >= selectionStart) {
+        if (newVisibleText[i].len > 0) {
+          if (newVisibleText[i].textType === 'text') {
+            newVisibleText.splice(i, 1, {
+              len: newVisibleText[i].len - 1,
+              textType: 'text'
+            })
+            break;
+          } else {
+            const left = value.substring(0, start);
+            const right = value.substring(start + newVisibleText[i].len)
+            setValue(left + right);
+            newVisibleText.splice(i, 1);
+            break;
+          }
+        }
+      }
+      start += newVisibleText[i].len;
+    }
+    setVisibleText(newVisibleText)
+  }
+
   const handleInputChange = (event) => {
     // TODO: We need special rules for deletion. We need to scan through the items and check if the deletion index lies within a non-text item.
     //       If so, we must delete it in its entireity and merge the adjacent text blocks. Otherwise we just perform a normal delete at the given index by
     //       finding the textbox that contains the index.
     const inputValue = event.target.value;
     setValue(inputValue);
-    updateBackgroundBoxes(event);
-    // console.log(inputValue, visibileText)
+    // if we are inserting
+    if (inputValue.length > value.length) {
+      updateBackgroundBoxes(event);
+    }
 
     if (showSuggestions) {
       // must use `inputValue`, otherwise race condition. + 1 to move past the "@" symbol
@@ -138,7 +170,6 @@ function MyTextbox() {
   }
 
   const handleKeyDown = (event) => {
-    // console.log(event.keyCode)
     if (event.keyCode === 13) { // enter key
       flushSuggestion(event.target.selectionStart)
     } else if (event.keyCode === 9) { // tab
@@ -155,7 +186,7 @@ function MyTextbox() {
     } else if (event.keyCode === 32) { // space (disables special behaviour)
       clearSuggestion();
     } else if (event.keyCode === 8) {// delete
-      // 
+      handleDelete(event);
     }
   }
 
@@ -192,7 +223,7 @@ function MyTextbox() {
   }
 
   const getVisibleTextWithStart = () => {
-    let visibleTextCopy = visibileText.slice();
+    let visibleTextCopy = JSON.parse(JSON.stringify(visibileText));
     let start = 0;
     for (let i=0; i<visibleTextCopy.length; ++i) {
       visibleTextCopy[i].start = start;
