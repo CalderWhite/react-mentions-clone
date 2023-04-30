2ae4bcd3daa11217cdd7fcf67465c9b776e11b73
Calder White <calderwhite1@gmail.com>
2023-04-30 14:39:52 -0400

    Switched to nextjs.

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyTextbox from '../components/MyTextbox';
+
+export default function Home() {
+  return (
+    <>
+      <p>Hi</p>
+      <MyTextbox />
+    </>
+  )
+}
