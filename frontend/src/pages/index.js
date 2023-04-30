090d849174764396c6c5c7ab6898ba25ad9f090a
Calder White <calderwhite1@gmail.com>
2023-04-30 15:00:41 -0400

    Added title.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MyTextbox from '../components/MyTextbox';
+import Head from 'next/head'
+
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>React-Mentions Demo</title>
+      </Head>
       <p>Use the textbox below! Type "@" to mention someone and either hit tab or enter to select your entry. You may also use the arrow keys to move between entries</p>
       <MyTextbox />
     </>
